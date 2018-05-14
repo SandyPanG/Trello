0714b09a03f3147829496ce98cba3f9e8c0f1ae3
SandyPanG <soleibiza@hotmail.com>
2018-05-14 13:21:01 -0500

    primera función

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var input = document.getElementById('list')
+input.addEventListener('click',function () {
+  console.log(input);
+  var listName = document.getElementById('list-name')
+  listName.style.display = 'block';
+})
