c60affaee1e731e3d69747315e564e57d8f069bf
SandyPanG <soleibiza@hotmail.com>
2018-05-15 13:51:00 -0500

    limpiar input despué de enviar

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
-var input = document.getElementById('list')
+var input = document.getElementById('list');
 input.addEventListener('click',function () {
   console.log(input);
   var listName = document.getElementById('list-name')
   listName.style.display = 'block';
-  
+
+var save = document.getElementById('save')
+save.addEventListener('click', function(){
+
+  var namelist = document.getElementById('list').value;
+  //limpiar el textarea
+	document.getElementById('list').value = '';
+})
+
+
 })
