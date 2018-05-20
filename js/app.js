553e1125596b4d54c26732b6c7dc5d4e1ba039dd
SandyPanG <soleibiza@hotmail.com>
2018-05-19 19:01:07 -0500

    intento de guardar texto ingresado

--- js/app.js
+++ js/app.js
@@ -1,16 +1,65 @@
-var input = document.getElementById('list');
+//Añadir lista
+var input = document.getElementById('list')
 input.addEventListener('click',function () {
   console.log(input);
   var listName = document.getElementById('list-name')
   listName.style.display = 'block';
+})
 
-var save = document.getElementById('save')
+var save = document.getElementById('list');
 save.addEventListener('click', function(){
+//tomar el texto ingresado en el textarea
+newList = document.getElementById('list').value;
+//limpiar el textarea
+newList = document.getElementById('list').value = '';
+//llamar a nuevo div
+  var message = document.getElementById('msg');
 
-  var namelist = document.getElementById('list').value;
+//nueva lista(el h4)
+  var title = document.createElement('h4');
+  var textNewList = document.createTextNode(newList);
+  var listName = document.getElementById('msg');
+  title.appendChild(textNewList);
+  msg.appendChild(title);
+});
+
+
+
+
+/*
+//función del botón guardar
+var save = document.getElementById('save');
+save.addEventListener('click', function(){
+  var newlist = document.getElementById('list').value;
   //limpiar el textarea
 	document.getElementById('list').value = '';
+
+
+
+
+
+
+ //contenedor que esta en el html
+var msg = document.getElementById('msg');
+
+  //creamos el div que contiene cada comentario
+
+	var newComments = document.createElement('div');
+
+  //nodos de texto del textarea
+
+	var textNewComment = document.createTextNode(namelist);
+
+
+
+	var contenedorElemento = document.createElement('p');
+
+	contenedorElemento.appendChild(textNewComment);
+
+	newComments.appendChild(contenedorElemento);
+	cont.appendChild(newComments);
 })
 
 
 })
+*/
